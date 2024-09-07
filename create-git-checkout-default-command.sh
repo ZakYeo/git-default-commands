304bd7c6aa98dbfa4c96189c04769c09df724102
ZakYeo <zakyeomanson@googlemail.com>
2024-09-07 15:50:18 +0100

    Added create-git-checkout-default-command.sh -> Similar to merge-default , this one will work for checking out

---
+++ create-git-checkout-default-command.sh
@@ -0,0 +1,4 @@
+git config --global alias.checkout-default '!f() { \
+  default_branch=$(git remote show origin | grep "HEAD branch" | awk "{print \$NF}"); \
+  git checkout $default_branch; \
+}; f'
