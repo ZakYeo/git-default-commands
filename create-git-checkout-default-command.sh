fdddf44a96796855b1ea8c1f1336ab299f806657
ZakYeo <zakyeomanson@googlemail.com>
2024-09-07 15:54:28 +0100

    Added missing shebangs for .sh files

--- create-git-checkout-default-command.sh
+++ create-git-checkout-default-command.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git config --global alias.checkout-default '!f() { \
   default_branch=$(git remote show origin | grep "HEAD branch" | awk "{print \$NF}"); \
   git checkout $default_branch; \
--- create-git-merge-default-command.sh
+++ create-git-merge-default-command.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git config --global alias.merge-default '!f() { \
   default_branch=$(git remote show origin | grep "HEAD branch" | awk "{print \$NF}"); \
   git merge origin/$default_branch; \
